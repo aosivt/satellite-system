c8ac0da21af33fccac0ab36783706848e5940049
ayuoshchepkov <ayuoshchepkov@kmr.beeline.ru>
2021-08-24 16:10:03 +0700

    add test for simple actor mls

--- src/test/scala/org/satellite/system/core/actors/TestMlsActor.scala
+++ src/test/scala/org/satellite/system/core/actors/TestMlsActor.scala
@@ -34,6 +34,32 @@ class TestMlsActor extends TestKit(ActorSystem("MySpec"))
       probe1.expectMsg(500 millis, "template")
       probe2.expectMsg(500 millis, "template")
     }
+    "test line regression" in {
+      val X = List(1D,2D,3D,4D,5D,6D)
+      val Y = List(12D,22D,31D,44D,55D,66D)
+      val SumXi = X.sum
+      val SumYi = Y.sum
+      val SqrSumXi = X.map(x=>Math.pow(x,2)).sum
+      val SqrSumYi = Y.map(y=>Math.pow(y,2)).sum
+      val XiYiSum = X.zip(Y).map((l: (Double, Double)) => l._1 * l._2).sum
+
+      val A = ((SumXi * SumYi) - (X.length*XiYiSum))/(Math.pow(SumXi,2)-(X.length*SqrSumXi))
+      val B = (SumXi*XiYiSum - (SqrSumXi*SumYi))/(Math.pow(SumXi,2)-(X.length*SqrSumXi))
+
+      println(s"> SumXi : ${SumXi}")
+      println(s"> SumYi : ${SumYi}")
+      println(s"> SqrSumXi : ${SqrSumXi}")
+      println(s"> SqrSumYi : ${SqrSumYi}")
+      println(s"> XiYiSum : ${XiYiSum}")
+      println(s"> A : ${A}")
+      println(s"> B : ${B}")
+
+      val Yreg = X.map(x=>A*x-B)
+
+      Yreg.foreach(println)
+
+    }
+
 
   }
 }
