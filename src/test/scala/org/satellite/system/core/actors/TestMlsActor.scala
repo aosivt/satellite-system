fc1dae3e379c33734807fd37dc9bc1aefc34d6ef
ayuoshchepkov <ayuoshchepkov@kmr.beeline.ru>
2021-08-26 14:20:41 +0700

    create test cramer method for matrix

--- src/test/scala/org/satellite/system/core/actors/TestMlsActor.scala
+++ src/test/scala/org/satellite/system/core/actors/TestMlsActor.scala
@@ -154,6 +154,131 @@ class TestMlsActor extends TestKit(ActorSystem("MySpec"))
       println(R2)
       println(Asr)
     }
+    "test cramer def" in {
+      val X = List(1D,2D,3D,4D,5D,6D)
+      val Y = List(12D,22D,31D,44D,55D,66D)
 
+      val SumXi = X.sum
+      val SumYi = Y.sum
+      val SqrSumXi = X.map(x=>Math.pow(x,2)).sum
+      val SumXi3 = X.map(x=>Math.pow(x,3)).sum
+      val XiYiSum = X.zip(Y).map((l: (Double, Double)) => l._1 * l._2).sum
+      val Xi2YiSum = X.zip(Y).map((l: (Double, Double)) => Math.pow(l._1,2) * l._2).sum
+
+      val Ma = Array.ofDim[Double](3,3)
+      Ma(0) = Array(1D,2D,3D)
+      Ma(1) = Array(4D,5D, 6D)
+      Ma(2) = Array(7D,8D,9D)
+
+      val M = Array.ofDim[Double](2,2)
+      M(0) = Array(1D,2D)
+      M(1) = Array(3D,4D)
+
+
+      def cramer: (Int, Array[Array[Double]])=> Double = (i, m) => {
+        m.length match {
+          case 1 => m(0)(0)
+          case _ =>
+            val nextI = i+1
+            val A:Array[Array[Double]] = m.drop(1).map(a=>{
+              val template = a.take(i) ++ a.drop(i+1)
+              template
+            })
+            val rootElement = (if (i%2==0)  m(0)(i) else - m(0)(i)) * cramer(0, A)
+            rootElement + (if (nextI==m.length) 0 else cramer(nextI, m))
+          }
+      }
+
+      println(s"result cramer : ${cramer(0,M)}")
+    }
+  }
+  "test cub regression" in {
+    //      1 2 3 4 5 6
+    //      12 22 31 44 55 66
+    val X = List(1D,2D,3D,4D,5D,6D)
+    val Y = List(12D,22D,31D,44D,55D,66D)
+
+    val SumXi = X.sum
+    val SumYi = Y.sum
+    val SqrSumXi = X.map(x=>Math.pow(x,2)).sum
+    val SumXi3 = X.map(x=>Math.pow(x,3)).sum
+    val SumXi4 = X.map(x=>Math.pow(x,4)).sum
+    val SqrSumYi = Y.map(y=>Math.pow(y,2)).sum
+    val XiYiSum = X.zip(Y).map((l: (Double, Double)) => l._1 * l._2).sum
+    val Xi2YiSum = X.zip(Y).map((l: (Double, Double)) => Math.pow(l._1,2) * l._2).sum
+
+    val A = ((SumXi * SumYi) - (X.length*XiYiSum))/(Math.pow(SumXi,2)-(X.length*SqrSumXi))
+    val B = (SumXi*XiYiSum - (SqrSumXi*SumYi))/(Math.pow(SumXi,2)-(X.length*SqrSumXi))
+
+    println(s"$SqrSumXi a + $SumXi b + ${X.length} c = $SumYi" )
+    println(s"$SumXi3 a + $SqrSumXi b + $SumXi c = $XiYiSum" )
+    println(s"$SumXi4 a + $SumXi3 b + $SqrSumXi c = $Xi2YiSum" )
+
+    val M = Array.ofDim[Double](3,3)
+    M(0) = Array(SqrSumXi,SumXi,X.length)
+    M(1) = Array(SumXi3,SqrSumXi,SumXi)
+    M(2) = Array(SumXi4,SumXi3,SqrSumXi)
+
+    val Ma = Array.ofDim[Double](3,3)
+    Ma(0) = Array(SumYi,SumXi,X.length)
+    Ma(1) = Array(XiYiSum,SqrSumXi,SumXi)
+    Ma(2) = Array(Xi2YiSum,SumXi3,SqrSumXi)
+
+    val Mb = Array.ofDim[Double](3,3)
+    Mb(0) = Array(SqrSumXi,SumYi,X.length)
+    Mb(1) = Array(SumXi3,XiYiSum,SumXi)
+    Mb(2) = Array(SumXi4,Xi2YiSum,SqrSumXi)
+
+    val Mc = Array.ofDim[Double](3,3)
+    Mc(0) = Array(SqrSumXi,SumXi,SumYi)
+    Mc(1) = Array(SumXi3,SqrSumXi,XiYiSum)
+    Mc(2) = Array(SumXi4,SumXi3,Xi2YiSum)
+
+
+    def cramer: (Int, Array[Array[Double]])=> Double = (i, m) => {
+      m.length match {
+        case 1 => m(0)(0)
+        case _ =>
+          val nextI = i+1
+          val A:Array[Array[Double]] = m.drop(1).map(a=>{
+            val template = a.take(i) ++ a.drop(i+1)
+            template
+          })
+          val rootElement = (if (i%2==0)  m(0)(i) else - m(0)(i)) * cramer(0, A)
+          rootElement + (if (nextI==m.length) 0 else cramer(nextI, m))
+      }
+    }
+
+    val DM = cramer(0,M)
+    val DMa = cramer(0,Ma)
+    val DMb = cramer(0,Mb)
+    val DMc = cramer(0,Mc)
+
+    val a = DMa/DM
+    val b = DMb/DM
+    val c = DMc/DM
+
+    val Yrg = X.map(x=>a*Math.pow(x,2) + b * x + c)
+    Yrg.foreach(println)
+    val Ysr = (1D/X.length)*SumYi
+    val YsrY = Y.map(y=>y-Ysr)
+    val YsrYSum = YsrY.sum
+    val YsrY2 = YsrY.map(y=>Math.pow(y,2))
+    val YsrYSum2 = YsrY2.sum
+    val YrgY = Y.zip(Yrg).map(y=>y._1-y._2)
+    val YrgY2 = YrgY.map(y=>Math.pow(y,2))
+    val YrgY2Sum = YrgY2.sum
+    //            index correlation
+    val R = Math.sqrt((1-(YrgY2Sum/YsrYSum2)))
+    //            index determination
+    val R2 = Math.pow(R,2)
+
+    val sumTest = (Y.zip(Yrg).map(y=>Math.abs(y._1-y._2)/y._1))
+    //      Average approximation error
+    val Asr = ((1D/X.length)*sumTest.sum) * 100
+
+    println(R)
+    println(R2)
+    println(Asr)
   }
 }
