e4949c020b990ea4e9aa3ea822576225e2fa3e7f
alex <ghost.alex86@gmail.com>
2021-11-06 01:48:51 +0700

    add watcher

--- image-converter/src/main/java/org/satellite/system/image/converter/Main.java
+++ image-converter/src/main/java/org/satellite/system/image/converter/Main.java
@@ -5,15 +5,16 @@ import org.satellite.system.image.converter.core.OptionsImageConverters;
 import org.satellite.system.image.converter.services.WatcherFileArchive;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         gdal.AllRegister();
-        init();
+        init(args.length > 0 ? args[0] : null);
     }
 
-    private static void init() throws IOException {
-        final var prop = new OptionsImageConverters();
+    private static void init(final String path) throws IOException {
+        final var prop = new OptionsImageConverters(path);
         WatcherFileArchive.start(prop);
     }
 
